4885a1f99b747aa4ab999f912874017831b8e6d0
Perl0ovkA <104366771+Perl0ovkA@users.noreply.github.com>
2024-06-26 17:28:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Enter first word: ";
+    std::string first;
+    std::cin >> first;
+    
+
+    std::cout << "The word: " << first << "\n" << "Number of letters: " << first.length() << "\n" <<
+       "The first letter: " << first.front() << "\n" << "The last letter: " << first.back() << "\n";
+
+    std::cin;
+    return 0;
+}
